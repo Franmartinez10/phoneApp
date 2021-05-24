3661934b1c47fd1612f467ed05f9954f033105e8
fran martinez torres <fran.martinez.torres@gmail.com>
2021-05-24 08:39:55 -1200

    d

--- src/app/server.js
+++ src/app/server.js
@@ -9,4 +9,4 @@ app.get('/*',function(req,res){
      res.sendFile(path.join(dirname + '/dist/papp/index.html')); */
 });
 
-app.listen(process.env.PORT  || 5000);
+app.listen(process.env.PORT  || 8080);
