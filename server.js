5d2f4c054619b1908f06a5cc573ec907cc307c3d
fran martinez torres <fran.martinez.torres@gmail.com>
2021-05-24 04:03:21 -1200

    d

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(dirname + '/dist/phoneApp'));
+app.use(express.static(dirname + '/dist/phone-app'));
 app.get('/*',function(req,res){
-/*     res.sendFile("index.html", {root:dirname + '/dist/phoneApp/'});
- */
-/*      res.sendFile(path.join(dirname + '/dist/index.html'));
- */    res.sendFile(path.join(dirname + '/dist/phoneApp/index.html'));
+   res.sendFile("index.html", {root:dirname + '/dist/phone-app/'});
+ 
+ /*  res.sendFile(path.join(dirname + '/dist/index.html'));
+     res.sendFile(path.join(dirname + '/dist/phone-app/index.html')); */
 });
 
 app.listen(process.env.PORT  || 8080);
