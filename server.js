1a70b0c4a8cac7070b2ab73af02748b5e9cd1882
fran martinez torres <fran.martinez.torres@gmail.com>
2021-05-24 21:18:55 -1200

    f

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(dirname + '/dist/phone-app'));
+app.use(express.static(dirname + '/__dist/phone-app'));
 app.get('/*',function(req,res){
-   res.sendFile("index.html", {root:dirname + '/dist/phone-app/'});
+   res.sendFile("index.html", {root:dirname + '/__dist/phone-app/'});
  
 /*     res.sendFile(path.join(dirname + '/dist/index.html'));
  res.sendFile(path.join(dirname + '/dist/phoneApp/index.html'));  */
